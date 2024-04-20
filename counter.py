41b8f2958220259bd5ff04ce167092a4b0369e85
tlneff <neff.tyler@gmail.com>
2024-04-19 22:35:19 -0500

    added program logic

--- counter.py
+++ counter.py
@@ -1,9 +1,18 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    lines = []
+    days = {}
+    with open(file_name) as file:
+        for line in file:
+            lines.append(line)
+    for line in lines:
+        if line[:5] == "From ":
+            day = line.split(' ')[2]
+            days[day] = days.get(day, 0)+1
+    print(days)
 
 
-
-## if you want to test locally run > python payCalculator.py
+# if you want to test locally run > python payCalculator.py
 if __name__ == "__main__":
     countDayOfTheWeek()
